fc9860b492fe626c91505fabbb7cc3e7c19eaa10
unknown <sihu714@gmail.com>
2026-05-18 10:19:25 +0900

    미들웨어

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,6 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    "/",
-    "/transactions",
-    "/analysis",
-    "/budget",
-    "/api/categories/:path*",
-    "/api/transactions/:path*",
-    "/api/budgets/:path*",
-    "/api/dashboard/:path*",
-    "/api/analysis/:path*",
+    "/((?!api/auth|login|register|_next/static|_next/image|favicon.ico).*)",
   ],
 };
